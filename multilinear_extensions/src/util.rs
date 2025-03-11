a8f20dfb31e679788acdc3b1d1bca544fa488675
MuhtasimTanmoy <tanmoy@abgpocket.com>
2025-03-11 17:06:43 +0600

    refactor

--- multilinear_extensions/src/util.rs
+++ multilinear_extensions/src/util.rs
@@ -1,4 +1,7 @@
 /// Decompose an integer into a binary vector in little endian.
+/// Input (u64)	num_var	Binary (little-endian)	Output (Vec<bool>)
+/// 5	3	101	[true, false, true]
+/// 13	3	101 (from 1101)	[true, false, true]
 pub fn bit_decompose(input: u64, num_var: usize) -> Vec<bool> {
     let mut res = Vec::with_capacity(num_var);
     let mut i = input;
